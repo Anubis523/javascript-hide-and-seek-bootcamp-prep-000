9e091806993cf637dab2c600cb8c19d55cd891e7
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 14:45:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("div#nested div.target")
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll("ul.ranked-list li")
+  console.log (ranks)
+  for (let i = 0; i < ranks.length; i++) {
+    let num = parseInt(ranks[i].innerHTML)
+    num += n
+    ranks[i].innerHTML = num
+  }
+}
+
+function deepestChild() {
+  let child  = document.querySelector("div#grand-node")
+  for (let i = 0; i < 4; i++){
+    child = child.querySelector('div')
+  }
+
+  return child
+}
