7438eaba6eb19d9ac747782836f1432627fe226f
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 14:07:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const ranks = document.querySelectAll("ul.rankedlist li")
+  const lists = document.querySelectAll("ul.rankedlist")
+  let ranks = []
+  
   for (let i = 0; i < ranks.length; i++) {
     let num = parseInt(ranks[i].innerHtml)
     num += n
