b1d612fa095bf35b606a24e50e55d20030b3dee3
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 14:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const ranks = document.querySelectAll("ul.rankedlist li")
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHtml ++;
+  }
 }
 
 function deepestChild() {
